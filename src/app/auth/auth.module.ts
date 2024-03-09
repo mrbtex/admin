240bbf34ab0a2f10792d01691ef999307e1b173b
mrbtextil <mrbtextil@gmail.com>
2024-03-09 15:57:34 -0500

    change auth,module.ts

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { LayoudPageComponent } from './layoud-page/layoud-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { LayoudPageComponent } from './pages/layoud-page/layoud-page.component';
 
 
 @NgModule({
